109b444b5b623e513551abc7f87aa5538bf174de
wonduck-hub <dnjsejr417@naver.com>
2024-03-22 15:53:09 +0900

    ProjectAssistant 솔루션에서 사용할 웹 및 api등의 프로젝트 생성

---
+++ ProjectAssistant.Models/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace ProjectAssistant.Models;
+
+[TestClass]
+public class UnitTest1
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+    }
+}
---
+++ ProjectAssistant/Pages/About.razor.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace ProjectAssistant.Pages
+{
+    partial class About : ComponentBase
+    {
+        private string title = "About";
+        private string Title { 
+            get { return title; }
+            set { title = value; }
+        }
+    }
+}
