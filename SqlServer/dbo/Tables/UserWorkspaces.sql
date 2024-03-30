d53530c996e1ee39080dd079d7a3e886ea7d617c
장원덕 <dnjsejr417@outlook.com>
2024-03-30 17:19:54 +0900

    SqlServer프로젝트 새성

---
+++ SqlServer/dbo/Tables/UserWorkspaces.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[UserWorkspaces]
+(
+	[AspNetUsersId] NVARCHAR(450) NOT NULL,
+	[WorkspaceId] INT NOT NULL,
+	PRIMARY KEY CLUSTERED ([AspNetUsersId] ASC, [WorkspaceId] ASC),
+	FOREIGN KEY ([WorkspaceId]) REFERENCES [dbo].[Workspaces] ([Id]),
+	FOREIGN KEY ([AspNetUsersId]) REFERENCES [dbo].AspNetUsers ([Id]),
+)
---
+++ SqlServer/dbo/Tables/Workspaces.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Workspaces]
+(
+	[Id] INT IDENTITY (1, 1) NOT NULL PRIMARY KEY,
+	[Name] NVARCHAR(255) NOT NULL,
+	[CreateUserId] NVARCHAR(450) NULL,
+	[ModifiedUserId] NVARCHAR(450) NULL,
+	[Created] DATETIMEOFFSET (7) DEFAULT ((sysdatetimeoffset() AT TIME ZONE 'Korea Standard Time')) NULL,
+	[Modified] DATETIMEOFFSET (7) NULL,
+)
