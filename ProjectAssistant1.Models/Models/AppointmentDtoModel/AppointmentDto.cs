20de87390343ec55e11f4bb2e260c47fe379384c
장원덕 <dnjsejr417@outlook.com>
2024-10-23 01:15:18 +0900

    Schedule에 현제 할당된 작업 표시 기능 추가

---
+++ ProjectAssistant1.Models/Models/AppointmentDtoModel/AppointmentDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectAssistant1.Models.Models.AppointmentDtoModel
+{
+    public class AppointmentDto
+    {
+        public string Title { get; set; }
+        public DateTime Start { get; set; }
+        public DateTime End { get; set; }
+        public string Color { get; set; }
+    }
+}
