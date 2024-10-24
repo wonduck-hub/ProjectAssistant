fe8a60a26aea0dbab68751417a5a722785b55e6c
장원덕 <dnjsejr417@outlook.com>
2024-10-24 21:39:27 +0900

    Schedulr 기능 추가(미완성: 아직 Workspace에서 자신에게 할당된 일정만 표시됨)

---
+++ ProjectAssistant1/AppointmentClass/MyAppointment.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+using BlazorScheduler;
+
+namespace ProjectAssistant1.AppointmentClass
+{
+    public class MyAppointment : Appointment
+    {
+        public bool IsWorkspaceWork { get; set; }
+
+    }
+}
---
+++ SqlServer/dbo/Tables/PersonalSchedule.sql
@@ -0,0 +1,17 @@
+﻿CREATE TABLE [dbo].[PersonalSchedule]
+(
+	[Id] INT IDENTITY (1, 1) NOT NULL PRIMARY KEY,
+	[Name] NVARCHAR(255) NOT NULL,
+	[StartDate] DATETIMEOFFSET (7) DEFAULT ((sysdatetimeoffset() AT TIME ZONE 'Korea Standard Time')) NULL,
+	[EndDate] DATETIMEOFFSET (7) DEFAULT ((sysdatetimeoffset() AT TIME ZONE 'Korea Standard Time')) NULL,
+	[IsSuccess] BIT DEFAULT(0) NOT NULL,
+	[SuccessDate] DATETIMEOFFSET (7) NULL,
+	[CreateUserId] NVARCHAR(450) NULL,
+	[ModifiedUserId] NVARCHAR(450) NULL,
+	[TaskDetails] NVARCHAR(500) NULL,
+	[Created] DATETIMEOFFSET (7) DEFAULT ((sysdatetimeoffset() AT TIME ZONE 'Korea Standard Time')) NULL,
+	[Modified] DATETIMEOFFSET (7) NULL,
+	[UserId] NVARCHAR (450) NOT NULL,
+	[IsDeleted]      BIT                NOT NULL DEFAULT 0,
+	FOREIGN KEY (UserId) REFERENCES AspNetUsers(Id)
+)
